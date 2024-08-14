9319aa0eef6b46d61f2b73161486d5b76f1abcf3
saddam311291 <saddam311291@gmail.com>
2024-08-14 19:57:54 +0600

    start portfolio

--- app/ui/common/popup.tsx
+++ app/ui/common/popup.tsx
@@ -1,9 +1,15 @@
 "use client";
 import Image from 'next/image';
 
-export function Popup({ isPopupOpen, togglePopup, imageSrc }) {
-  if (!isPopupOpen) return null;
+type PopupProps = {
+  isPopupOpen: boolean;
+  togglePopup: () => void;
+  imageSrc: string;
+};
 
+export function Popup({ isPopupOpen, togglePopup, imageSrc }: PopupProps) {
+  if (!isPopupOpen) return null;
+  
   return (
     <div
       className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50"
